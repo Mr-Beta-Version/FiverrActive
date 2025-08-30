2cd8fcd8aa5631ac000ba2722f54ebe549ac03e5
Mr. Beta <94112333+Mr-Beta-Version@users.noreply.github.com>
2025-08-30 12:57:47 +0600

    Delete refresh.py

--- refresh.py
+++
@@ -1,9 +0,0 @@
-import os,time,random 
-
-while True:
-    os.system("am start -n com.walid.far/.MainActivity")
-    time.sleep(random.randint(5,10))
-    os.system("am start -n com.walid.back/.MainActivity")
-    time.sleep(random.rantint(10,50))
-
-
